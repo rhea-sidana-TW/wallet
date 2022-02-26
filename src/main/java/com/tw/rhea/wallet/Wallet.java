039212891f62e7c563eaff935133c7d1e883c09d
rhea-sidana-TW <rhea.sidana@thoughtworks.com>
2022-02-26 16:30:10 +0530

    [Rhea]: Add: 'test where credited money should not be less than one'

--- src/main/java/com/tw/rhea/wallet/Wallet.java
+++ src/main/java/com/tw/rhea/wallet/Wallet.java
@@ -1,5 +1,7 @@
 package com.tw.rhea.wallet;
 
+import com.tw.rhea.wallet.Exception.MoneyLessThanOneException;
+
 public class Wallet {
     private double money;
 
@@ -7,7 +9,8 @@ public class Wallet {
         this.money = money;
     }
 
-    public void credit(double money) {
+    public void credit(double money) throws MoneyLessThanOneException {
+        if(money<1.0) throw new MoneyLessThanOneException();
         this.money += money;
     }
 
--- src/test/java/com/tw/rhea/wallet/WalletTest.java
+++ src/test/java/com/tw/rhea/wallet/WalletTest.java
@@ -1,19 +1,28 @@
 package com.tw.rhea.wallet;
 
+import com.tw.rhea.wallet.Exception.MoneyLessThanOneException;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WalletTest {
     @Test
-    void shouldBeAbleToCreditMoneyInTheWallet() {
+    void shouldBeAbleToCreditMoneyInTheWallet() throws MoneyLessThanOneException{
         Wallet wallet = new Wallet(100);
 
         wallet.credit(200);
         double actual = 300.0;
 
         assertThat(actual, is(closeTo(wallet.checkBalance(), 0.001)));
+    }
+
+    @Test
+    void shouldNotBeAbleToCreditMoneyWhenCreditedMoneyIsLessThanOne() {
+        Wallet wallet = new Wallet(100);
+
+        assertThrows(MoneyLessThanOneException.class,()->wallet.credit(0));
     }
 }
